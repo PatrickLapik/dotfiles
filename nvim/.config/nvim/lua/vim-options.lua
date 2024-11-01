ce702fa658d938bf59e2ea6cace5947c061f7671
PatrickLapik <patrick.lapimaa@tptlive.ee>
2024-11-01 22:40:01 +0200

    add tab spaces for gd script

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -13,4 +13,12 @@ map("i", "<C-k>", "<Up>", {})
 map("i", "<C-z>", "<c-o>u", {})
 vim.g.mapleader = " "
 
-
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "gdscript", 
+    callback = function()
+        vim.opt_local.expandtab = false
+        vim.opt_local.tabstop = 4
+        vim.opt_local.softtabstop = 4
+        vim.opt_local.shiftwidth = 4
+    end,
+})
